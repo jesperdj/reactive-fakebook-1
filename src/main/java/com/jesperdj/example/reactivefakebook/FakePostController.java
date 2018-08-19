904f848d88f3e6dfcd75cfedd78d274ce8dbfdd0
Jesper de Jong <jespdj@gmail.com>
2018-08-19 20:42:06 +0200

    Initial code commit

---
+++ src/main/java/com/jesperdj/example/reactivefakebook/FakePostController.java
@@ -0,0 +1,66 @@
+package com.jesperdj.example.reactivefakebook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/posts")
+public class FakePostController {
+
+    private final FakePostRepository repository;
+
+    @Autowired
+    public FakePostController(FakePostRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Flux<FakePost> getAll() {
+        return repository.findAllByOrderByTimestampDesc();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<FakePost>> getById(@PathVariable("id") String id) {
+        return repository.findById(id)
+                .map(post -> ResponseEntity.ok(post))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Mono<FakePost> create(@RequestBody FakePost post) {
+        post.setTimestamp(LocalDateTime.now());
+        return repository.save(post);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<FakePost>> update(@PathVariable("id") String id, @RequestBody FakePost post) {
+        return repository.findById(id)
+                .flatMap(existingPost -> {
+                    existingPost.setTimestamp(LocalDateTime.now());
+                    existingPost.setContent(post.getContent());
+                    return repository.save(existingPost);
+                })
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> delete(@PathVariable("id") String id) {
+        return repository.findById(id)
+                .flatMap(post -> repository.delete(post)
+                        .then(Mono.<ResponseEntity<Void>>just(ResponseEntity.ok().build())))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+}
